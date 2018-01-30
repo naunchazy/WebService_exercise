c7c1aec9f0b50dbb9b72031429725a8924f35025
Microsoft <Microsoft@Microsoft-PC>
2018-01-30 19:57:38 +0800

    CXF+Spring整合发布SOAP的服务【使用jaxws:server发布SOAP协议的服务】-服务端

---
+++ src/server2/WeatherInterface.java
@@ -0,0 +1,13 @@
+package server2;
+
+import javax.jws.WebService;
+import javax.xml.ws.BindingType;
+import javax.xml.ws.soap.SOAPBinding;
+
+
+@WebService
+@BindingType(SOAPBinding.SOAP12HTTP_BINDING)
+public interface WeatherInterface {
+
+	public String queryWeather(String cityName);
+}
