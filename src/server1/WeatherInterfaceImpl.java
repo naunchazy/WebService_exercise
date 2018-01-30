3566399fbb774803c5f01724b7c493db4d02f89e
Microsoft <Microsoft@Microsoft-PC>
2018-01-30 19:56:33 +0800

    CXF+Spring整合发布SOAP的服务【使用endpoint发布soap协议的服务】-服务端

---
+++ src/server1/WeatherInterfaceImpl.java
@@ -0,0 +1,16 @@
+package server1;
+
+public class WeatherInterfaceImpl implements WeatherInterface {
+
+	@Override
+	public String queryWeather(String cityName) {
+		String result=null;
+		if (cityName.equals("º¼ÖÝ")) {
+			result="¶àÔÆ×ªÒõ";
+		} else {
+			result="Çç";
+		}
+		return result;
+	}
+
+}
